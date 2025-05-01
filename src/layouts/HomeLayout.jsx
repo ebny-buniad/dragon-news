84013ec3adb3fc0c93be20eb8adf0b34aca5d2ef
ebnybuniad <ebnybuniad@hmail.com>
2025-05-01 13:36:10 +0600

    load category

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -3,6 +3,8 @@ import { Outlet } from 'react-router';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
+import LeftAside from '../components/homelayout/LeftAside';
+import RightAside from '../components/homelayout/RightAside';
 
 const HomeLayout = () => {
     return (
@@ -12,12 +14,16 @@ const HomeLayout = () => {
                 <LatestNews></LatestNews>
                 <Navbar></Navbar>
             </header>
-            <main>
-                <section className="left-nav"></section>
-                <section className="main">
+            <main className='w-10/12 mx-auto grid grid-cols-12 mt-5'>
+                <aside className='col-span-3'>
+                    <LeftAside></LeftAside>
+                </aside>
+                <section className="main col-span-6">
                     <Outlet></Outlet>
                 </section>
-                <section className="right-nav"></section>
+                <aside className='col-span-3'>
+                    <RightAside></RightAside>
+                </aside>
             </main>
         </>
     );
